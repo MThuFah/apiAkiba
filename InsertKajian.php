b9d8a4143d6d2b648b85e06a225f5ad4a9e143bd
mthufah <mukarramahthufah@gmail.com>
2018-10-27 23:34:20 +0800

    sisa notificasi remindernya kak -ama

--- InsertKajian.php
+++ InsertKajian.php
@@ -103,6 +103,21 @@ include 'koneksi.php' ;
         curl_close($ch);
         echo $response;
 
+        $fields2 = json_encode($fields2);
+//        dd($fields);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+            'Authorization: Basic '.$authorization));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        echo $response;
+
       
       if ($query) {
       $response = new emp();
